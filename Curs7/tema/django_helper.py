5dde10031343ecd47acefcef03a54fcbda033f3e
Laura Popescu <laura.liliana.popescu@gmail.com>
2025-04-25 20:02:46 +0300

    Functie setup app

--- Curs7/tema/django_helper.py
+++ Curs7/tema/django_helper.py
@@ -11,9 +11,13 @@ else:
     python_command ="python3"
 
 
-def create_project(project_name="test_project"):
+def create_project(project_name="test_project", *applications):
     CREATE_PROJECT_CMD = f"{python_command} -m django startproject {project_name}"
     subprocess.call(CREATE_PROJECT_CMD, shell = True)
+
+    for app_name in applications:
+        create_application(app_name, project_name)
+        
 
 def delete_project(project_name="test_project"):
     shutil.rmtree(project_name)
@@ -23,8 +27,19 @@ def create_application(app_name ="test_app", project_name="test_project"):
     cwd = os.getcwd()
     # imi schimba path-ul
     os.chdir(os.path.join(cwd, project_name))
+
     CREATE_APP_CMD = f"{python_command} manage.py startapp {app_name}"
     subprocess.call(CREATE_APP_CMD, shell = True)
+    time.sleep(0.5)
+    setup_application(app_name, project_name)
+
+def setup_application(app_name ="test_app", project_name="test_project"):
+    _add_app_to_installed_apps(app_name, project_name)
+    create_templates_folder(app_name, project_name)
+    _create_app_url_file(app_name, project_name)
+    _link_app_in_project_url_file(app_name, project_name)
+
+    
 
 def _add_app_to_installed_apps(app_name ="test_app", project_name="test_project"):
      #Current working directory
@@ -61,7 +76,7 @@ def create_templates_folder(app_name ="test_app", project_name="test_project"):
 
 #Creeaza urls.py
 
-def _create_urls_files(app_name ="test_app", project_name="test_project"):
+def _create_app_url_file(app_name ="test_app", project_name="test_project"):
     cwd = os.getcwd()
     os.chdir(os.path.join(cwd, project_name, app_name))
     URLS_FILE_NAME = "urls.py"
@@ -69,7 +84,28 @@ def _create_urls_files(app_name ="test_app", project_name="test_project"):
     with open(URLS_FILE_NAME, "w") as fwriter:
         fwriter.write(URLS_CONTENT)
 
-   
+def _link_app_in_project_url_file(app_name ="test_app", project_name="test_project"):
+    cwd = os.getcwd()
+    os.chdir(os.path.join(cwd, project_name, project_name))
+    with open("urls.py", "r") as freader:
+        urls_lines = freader. readlines()
+    has_encounter_urlpatterns = False
+    new_line = f"\tpath ('{app_name}/', include('{app_name}.urls')),\n"
+
+    if new_line in urls_lines:
+        return
+    
+    for index, line in enumerate(urls_lines):
+        if 'from django.urls import path' in line and 'include' not in line:
+            urls_lines[index] = line = line.replace("path", "path, include")
+        if "urlpatterns = [" in line:
+            has_encounter_urlpatterns = True
+        elif has_encounter_urlpatterns and "]" in line:
+            urls_lines.insert(index, new_line)
+            break
+    with open("urls.py", "w") as fwriter:
+        fwriter.writelines(urls_lines)
+
 
 if __name__ == "__main__":
     #print(os.getcwd())
@@ -81,8 +117,8 @@ if __name__ == "__main__":
     #_add_app_to_installed_apps()
     #create_templates_folder()
 
-    _create_urls_files()
-
+    #_create_app_url_file()
+    _link_app_in_project_url_file()
 
 
 
