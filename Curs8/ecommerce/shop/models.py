5fc4db4b58b026399adcadb78288eab93b2e8268
Laura Popescu <laura.liliana.popescu@gmail.com>
2025-04-30 20:54:58 +0300

    Un proiect si doua aplicatii

--- Curs8/ecommerce/shop/models.py
+++ Curs8/ecommerce/shop/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+###Product, title, description, slug
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(null = True)
+    slug = models.SlugField(null = True)
+
+    def __str__(self):
+        return self.title
--- Curs8/ecommerce/shop/views.py
+++ Curs8/ecommerce/shop/views.py
@@ -1,11 +1,22 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def product_list_view(request):
-	context = {}
+	product_model_list = Product.objects.all()
+	context = {
+		"product_model_list" : product_model_list
+	}
 	return render(request, 'product_list.html', context)
 
-def product_details_view(request):
-	context = {}
+# def product_details_view(request):
+# 	context = {}
+# 	return render(request, 'product_details.html', context)
+
+def product_details_view_with_slug(request,slug):
+	current_product_model = Product.objects.filter(slug = slug)[0]
+	context = {
+		"current_product_model": current_product_model
+	}
 	return render(request, 'product_details.html', context)
